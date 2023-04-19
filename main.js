c97a3619a0be02189b8219d037645ba420fb1fef
Chiragdbb <schirag414@gmail.com>
2023-04-19 19:34:25 +0530

    completed without styling

---
+++ main.js
@@ -0,0 +1,159 @@
+const range = document.querySelector("#range");
+const passLen = document.querySelector(".pass-len");
+const generateBtn = document.querySelector("#generate-btn");
+const checkedUppercase = document.querySelector("#uppercase");
+const checkedLowerase = document.querySelector("#lowercase");
+const checkedNumber = document.querySelector("#numbers");
+const checkedSymbols = document.querySelector("#symbols");
+const passwordDisplay = document.querySelector("#password");
+const copy = document.querySelector("#copy");
+const copyPopup = document.querySelector(".copy-text");
+const strengthColor = document.querySelector(".strength-color");
+const checkedInputs = document.querySelectorAll("input[type=checkbox]");
+
+let password = "";
+let passwordLength = 10;
+let totalChecked = 0;
+const symbols = "`~!@#$%^&*([]{'}\")_+=-,./<>?;:'";
+
+function handleSlider() {
+	passwordLength = range.value;
+	passLen.textContent = passwordLength;
+	return passwordLength;
+}
+
+range.addEventListener("input", handleSlider);
+
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getRandomUppercase() {
+	return String.fromCharCode(getRandomInt(65, 91));
+}
+
+function getRandomLowercase() {
+	return String.fromCharCode(getRandomInt(97, 123));
+}
+
+function getRandomNumber() {
+	return getRandomInt(0, 9);
+}
+
+function getRandomSymbol() {
+	let symbol = getRandomInt(0, symbols.length);
+	return symbols[symbol];
+}
+
+// passward strength
+
+function passStrength(str) {
+	let upperPresent = false;
+	let lowerPresent = false;
+	let numberPresent = false;
+	let symbolPresent = false;
+
+	if (checkedUppercase.checked) upperPresent = true;
+	if (checkedLowerase.checked) lowerPresent = true;
+	if (checkedNumber.checked) numberPresent = true;
+	if (checkedSymbols.checked) symbolPresent = true;
+
+	// strong
+	if (
+		upperPresent &&
+		lowerPresent &&
+		numberPresent &&
+		symbolPresent &&
+		str.length >= 8
+	) {
+		strengthColor.style.backgroundColor = "green";
+	}
+
+	//normal
+	if (str.length > 6 && str.length < 12 && !symbolPresent) {
+		strengthColor.style.backgroundColor = "yellow";
+	}
+
+	// weak
+	if (str.length < 6 || (!numberPresent && !symbolPresent)) {
+		strengthColor.style.backgroundColor = "red";
+	}
+}
+
+// clipboard copy
+async function copyClipboard() {
+	try {
+		await navigator.clipboard.writeText(passwordDisplay.value);
+		copyPopup.textContent = "Copied";
+	} catch (e) {
+		copyPopup.textContent = "Failed";
+	}
+
+	copyPopup.classList.add("active");
+
+	setTimeout(() => copyPopup.classList.remove("active"), 3000);
+}
+
+copy.addEventListener("click", () => {
+	if (password !== "") {
+		copyClipboard();
+	}
+});
+
+function shuffle(array) {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		const temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+	let str = "";
+	array.forEach((element) => (str += element));
+	return str;
+}
+
+// generate password
+generateBtn.addEventListener("click", () => {
+	password = "";
+	totalChecked = 0;
+
+	// min characters selected
+	let funcArr = [];
+	if (checkedUppercase.checked) {
+		funcArr.push(getRandomUppercase);
+	}
+	if (checkedLowerase.checked) {
+		funcArr.push(getRandomLowercase);
+	}
+	if (checkedNumber.checked) {
+		funcArr.push(getRandomNumber);
+	}
+	if (checkedSymbols.checked) {
+		funcArr.push(getRandomSymbol);
+	}
+
+	// compulsory characters
+	for (let i = 0; i < funcArr.length; i++) {
+		password += funcArr[i]();
+	}
+
+	// rest of characters
+	if (funcArr.length !== 0) {
+		for (let i = 0; i < passwordLength - funcArr.length; i++) {
+			let randInt = getRandomInt(0, funcArr.length);
+			password += funcArr[randInt]();
+		}
+	} else {
+		alert("Please select atleast one field");
+	}
+
+	// shuffle password
+	password = shuffle(Array.from(password));
+
+	// display
+	passwordDisplay.value = password;
+
+	// strength check
+	passStrength(password);
+	
+});
